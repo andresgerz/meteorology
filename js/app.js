17e46a25e1f6347828f1564fe1d11271eb2c8518
andresger <andres.gerz@gmail.com>
2018-11-06 01:03:25 -0300

    updated html, css and js. included new images file.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,32 @@
 console.log('Hola Matt Damon!');
+
+
+// I create an array 
+var images=["http://estaticos.smn.gob.ar/vmsr/goes16/nefo_00_24.jpg" ,
+"http://estaticos.smn.gob.ar/vmsr/goes16/mtoparg_24.jpg"
+]
+
+
+var num=0;
+
+function next(){
+  var slider=document.getElementById("slider");
+  
+  num++;
+  if (num>=images.length){
+    num=0;
+  }
+  slider.src=images[num]
+}
+
+
+function prev(){
+  var slider=document.getElementById("slider");
+
+  num--;
+
+  if(num < 0){
+    num=images.length-1;
+  }
+  slider.scr=images[num];
+}
